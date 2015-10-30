9cdd538dc81d85212735505f7e9502d0d55f49b5
Jörg Schlötterer <joerg.schloetterer@uni-passau.de>
2015-10-30 20:24:10 +0100

    minor

--- guessLanguage.js
+++ guessLanguage.js
@@ -32,7 +32,7 @@
  *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-define(['_languageData'], function(models) {
+define(['./_languageData'], function(models) {
 
   var guessLanguage = function() {
 
